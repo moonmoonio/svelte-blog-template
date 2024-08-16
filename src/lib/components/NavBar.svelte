70601edfea036e51ff17fe63001922dfebd01bd3
Luna <moonmoonio@proton.me>
2024-08-15 22:56:20 -0700

    Crate NavBar component

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,16 @@
+<script lang='ts'>
+	import NavHamburger from 'flowbite-svelte/NavHamburger.svelte'
+	import NavLi from 'flowbite-svelte/NavLi.svelte'
+	import NavUl from 'flowbite-svelte/NavUl.svelte'
+	import Navbar from 'flowbite-svelte/Navbar.svelte'
+	import {t, locale} from '$lib/translations'
+</script>
+
+<Navbar>
+	<NavHamburger>
+		<NavUl>
+			<NavLi href={`/${locale}`}>{$t('navBar.home')}</NavLi>
+			<NavLi href={`/${locale}/about`}>{$t('navBar.about')}</NavLi>
+		</NavUl>
+	</NavHamburger>
+</Navbar>
