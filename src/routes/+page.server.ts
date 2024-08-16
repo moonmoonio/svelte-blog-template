7e222a3bb661fc9328f50033eba41e0f01d980e9
Luna <moonmoonio@proton.me>
2024-08-15 22:57:31 -0700

    Redirect using default locale

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,7 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import { defaultLocale } from '$lib/translations';
 
 export const load: PageServerLoad = () => {
-	redirect(302, '/en');
+	redirect(302, `/${defaultLocale}`);
 };
