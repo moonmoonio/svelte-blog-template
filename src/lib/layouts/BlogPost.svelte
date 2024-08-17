f8cba2b907524ccc0922a61f4e0de06ed7867d02
Luna <moonmoonio@proton.me>
2024-08-17 16:01:37 -0700

    Improve blog post styling

--- src/lib/layouts/BlogPost.svelte
+++ src/lib/layouts/BlogPost.svelte
@@ -2,6 +2,33 @@
 	export let title;
 </script>
 
-<h1>{title}</h1>
+<div class="container mx-auto p-6 text-xl">
+	<div>
+		<h1>{title}</h1>
+		<slot />
+	</div>
+</div>
 
-<slot />
+<style>
+	div :global(h1) {
+		@apply text-4xl font-extrabold leading-relaxed md:text-5xl lg:text-6xl dark:text-white;
+	}
+	div :global(h2) {
+		@apply text-4xl font-extrabold leading-relaxed dark:text-white;
+	}
+	div :global(h3) {
+		@apply text-2xl font-extrabold leading-relaxed dark:text-white;
+	}
+	div :global(p) {
+		@apply py-1 text-xl leading-normal dark:text-white;
+	}
+	div :global(ul) {
+		@apply list-inside list-disc pl-6 text-xl leading-normal dark:text-white;
+	}
+	div :global(ol) {
+		@apply list-inside list-decimal pl-6 text-xl leading-normal dark:text-white;
+	}
+	div :global(pre) {
+		@apply text-xl dark:text-white;
+	}
+</style>
