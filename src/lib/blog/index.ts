cc1973bf0e40085106f61b05619e2e8be4b80226
Luna <moonmoonio@proton.me>
2024-08-17 15:01:14 -0700

    Create map of blog posts in lib

---
+++ src/lib/blog/index.ts
@@ -0,0 +1,18 @@
+import type { BlogPost } from '$lib/types/BlogPost';
+import type { BlogPosts } from '$lib/types/BlogPosts';
+
+const entries = import.meta.glob<BlogPost>('./posts/**/*.svx', { eager: true });
+
+const posts: BlogPosts = {};
+for (const path in entries) {
+	const post = entries[path];
+	let [locale, slug] = path.split('/').reverse();
+	locale = locale.slice(0, locale.indexOf('.'));
+	if (posts[locale]) {
+		posts[locale].set(slug, post);
+	} else {
+		posts[locale] = new Map([[slug, post]]);
+	}
+}
+
+export { posts };
