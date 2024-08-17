60624c5abd21db2e7ab6836f47d0ed5894e9177c
Luna <moonmoonio@proton.me>
2024-08-17 14:00:27 -0700

    Create blog page

---
+++ src/routes/[lang=langs]/blog/[slug=alnumString]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { locale } from '$lib/translations';
+
+	const blogPost = import(`$lib/blog/posts/${$page.params.slug}/${$locale}.svx`);
+	console.info($locale);
+</script>
+
+{#await blogPost then post}
+	<svelte:component this={post.default} />
+{/await}
