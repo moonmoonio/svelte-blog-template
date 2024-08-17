68f69b59f0b3539f4feafcebeea4850bf2fb14c4
Luna <moonmoonio@proton.me>
2024-08-16 18:35:23 -0700

    Use new translation files in lib

--- src/lib/translations/index.ts
+++ src/lib/translations/index.ts
@@ -12,14 +12,15 @@ import esNavBar from './es/navBar.json';
 
 // Language agnostic
 import generic from './generic.json';
+import languages from './languages.json';
 
 export const defaultLocale: string = Languages.EN;
 
 export const config: Config = {
 	initLocale: 'en',
 	translations: {
-		en: { navBar: enNavBar, generic },
-		es: { navBar: esNavBar, generic }
+		en: { navBar: enNavBar, generic, languages },
+		es: { navBar: esNavBar, generic, languages }
 	}
 };
 
