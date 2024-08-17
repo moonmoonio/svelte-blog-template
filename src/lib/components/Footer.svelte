bd4afcd0aa5eb77467db891aedc44bbb656d46dd
Luna <moonmoonio@proton.me>
2024-08-16 22:31:47 -0700

    Create Footer component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import Footer from 'flowbite-svelte/Footer.svelte';
+	import FooterCopyright from 'flowbite-svelte/FooterCopyright.svelte';
+	import FooterLink from 'flowbite-svelte/FooterLink.svelte';
+	import FooterLinkGroup from 'flowbite-svelte/FooterLinkGroup.svelte';
+
+	import { hrefConverter } from '$lib/transformers';
+	import { site_config } from '$lib/config/site';
+	import { t, locale } from '$lib/translations';
+
+	const year = new Date().getFullYear();
+</script>
+
+<hr />
+<Footer>
+	<div class="m-6 sm:flex sm:items-center sm:justify-between">
+		<FooterCopyright href={hrefConverter('/', $locale)} by={site_config().copyrightOwner} {year} />
+		<FooterLinkGroup
+			ulClass="flex flex-wrap items-center mt-3 text-sm ext-gray-500 dark:text-gray-400 sm:mt-0"
+		>
+			<FooterLink href={hrefConverter('/about', $locale)}>{$t('navBar.about')}</FooterLink>
+		</FooterLinkGroup>
+	</div>
+</Footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
 	import '../app.css';
 	import NavBar from '$lib/components/NavBar.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 </script>
 
 <NavBar />
 <main>
 	<slot />
 </main>
+<Footer />
