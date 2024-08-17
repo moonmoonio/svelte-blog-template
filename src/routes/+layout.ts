91064f69766048073216f2950b922e9d29922708
Luna <moonmoonio@proton.me>
2024-08-17 13:59:19 -0700

    Remove file

--- src/routes/+layout.ts
+++
@@ -1,8 +0,0 @@
-import { setLocale } from '$lib/translations';
-import type { LayoutLoad } from './$types';
-
-export const prerender = true;
-
-export const load: LayoutLoad = async ({ params }) => {
-	await setLocale(params.lang);
-};
