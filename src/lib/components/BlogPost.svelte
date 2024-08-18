29059b8912f985a32c067e38b11d1c9ecf1f67cb
Luna <moonmoonio@proton.me>
2024-08-17 19:09:31 -0700

    Blog post styling

--- src/lib/components/BlogPost.svelte
+++ src/lib/components/BlogPost.svelte
@@ -11,13 +11,13 @@
 
 <style lang="postcss">
 	div :global(h1) {
-		@apply text-4xl font-extrabold leading-relaxed md:text-5xl lg:text-6xl dark:text-white;
+		@apply text-4xl font-extrabold leading-relaxed md:text-6xl dark:text-white;
 	}
 	div :global(h2) {
-		@apply text-4xl font-extrabold leading-relaxed dark:text-white;
+		@apply text-2xl font-extrabold leading-relaxed md:text-4xl dark:text-white;
 	}
 	div :global(h3) {
-		@apply text-2xl font-extrabold leading-relaxed dark:text-white;
+		@apply text-xl font-extrabold leading-relaxed md:text-2xl dark:text-white;
 	}
 	div :global(p) {
 		@apply py-1 text-xl leading-normal dark:text-white;
