6cbdfae1b8f2e3b5cb96c836c16616e04481957a
Luna <moonmoonio@proton.me>
2024-08-17 20:43:58 -0700

    Store posts sorted by tag

--- src/lib/blog/index.ts
+++ src/lib/blog/index.ts
@@ -1,19 +1,33 @@
 import type { BlogPost } from '$lib/types/BlogPost';
 import type { BlogPosts } from '$lib/types/BlogPosts';
+import type { TagsPosts } from '$lib/types/TagsPosts';
 
 const entries = import.meta.glob<BlogPost>('./posts/**/*.svx', { eager: true });
 
 const posts: BlogPosts = {};
+const tags: TagsPosts = {};
 for (const path in entries) {
 	const post = entries[path];
 	post.metadata.publishDate = new Date(post.metadata.publishDate);
 	let [locale, slug] = path.split('/').reverse();
+	post.metadata.slug = slug;
 	locale = locale.slice(0, locale.indexOf('.'));
 	if (posts[locale]) {
 		posts[locale].set(slug, post);
 	} else {
 		posts[locale] = new Map([[slug, post]]);
 	}
+	post.metadata.tags?.forEach((tag) => {
+		if (tags[locale]) {
+			if (tags[locale].has(tag)) {
+				tags[locale].get(tag)?.push(post);
+			} else {
+				tags[locale].set(tag, [post]);
+			}
+		} else {
+			tags[locale] = new Map([[tag, [post]]]);
+		}
+	});
 }
 
 export { posts };
