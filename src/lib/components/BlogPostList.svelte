ba31ab2a15c88ae4194ab6e625fddbb59ab79e31
Luna <moonmoonio@proton.me>
2024-08-17 18:43:01 -0700

    Link to blog posts in blog list

--- src/lib/components/BlogPostList.svelte
+++ src/lib/components/BlogPostList.svelte
@@ -4,6 +4,7 @@
 	import Button from 'flowbite-svelte/Button.svelte';
 	import { ArrowRightOutline } from 'flowbite-svelte-icons';
 
+	import { hrefConverter } from '$lib/transformers';
 	import { locale } from '$lib/translations';
 	import type { BlogPosts } from '$lib/types/BlogPosts';
 
@@ -12,13 +13,15 @@
 
 <div class="container mx-auto px-6 py-6 text-xl md:px-72">
 	<Timeline>
-		{#each blogPosts[$locale].values() as post}
+		{#each blogPosts[$locale].entries() as [slug, post]}
 			<TimelineItem
 				title={post.metadata.title}
 				date={new Date(post.metadata.publishDate).toLocaleDateString($locale)}
 			>
 				<p class="mb-4 font-normal text-gray-500 dark:text-gray-400">Lorem ipsum</p>
-				<Button color="alternative">Read more<ArrowRightOutline class="ms-2 h-5 w-5" /></Button>
+				<Button color="alternative" href={hrefConverter(`/blog/${slug}`, $locale)}
+					>Read more<ArrowRightOutline class="ms-2 h-5 w-5" /></Button
+				>
 			</TimelineItem>
 		{/each}
 	</Timeline>
