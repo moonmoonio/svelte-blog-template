1d34df4078168b029a583a7f35ab42fd4d25d277
Luna <moonmoonio@proton.me>
2024-08-17 18:58:50 -0700

    Styling of blog post list

--- src/lib/components/BlogPostList.svelte
+++ src/lib/components/BlogPostList.svelte
@@ -11,18 +11,26 @@
 	export let blogPosts: BlogPosts;
 </script>
 
-<div class="container mx-auto px-6 py-6 text-xl md:px-72">
+<div class="container mx-auto px-6 py-6 text-xl lg:px-52">
 	<Timeline>
 		{#each blogPosts[$locale].entries() as [slug, post]}
-			<TimelineItem
-				title={post.metadata.title}
-				date={new Date(post.metadata.publishDate).toLocaleDateString($locale)}
-			>
-				<p class="mb-4 font-normal text-gray-500 dark:text-gray-400">Lorem ipsum</p>
-				<Button color="alternative" href={hrefConverter(`/blog/${slug}`, $locale)}
-					>Read more<ArrowRightOutline class="ms-2 h-5 w-5" /></Button
+			<div class="m-6 flex w-auto items-center rounded-lg bg-gray-50 py-2 dark:bg-gray-900">
+				<TimelineItem
+					title={post.metadata.title}
+					date={new Date(post.metadata.publishDate).toLocaleDateString($locale)}
 				>
-			</TimelineItem>
+					<p class="mb-4 font-normal text-gray-500 dark:text-gray-400">Lorem ipsum</p>
+					<Button color="alternative" href={hrefConverter(`/blog/${slug}`, $locale)}
+						>Read more<ArrowRightOutline class="ms-2 h-5 w-5" /></Button
+					>
+				</TimelineItem>
+			</div>
 		{/each}
 	</Timeline>
 </div>
+
+<style lang="postcss">
+	div :global(h3) {
+		@apply text-2xl md:text-4xl;
+	}
+</style>
