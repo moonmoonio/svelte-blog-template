6caf74285d8bdee875ee06ece1b48ffa1f7fcc03
Luna <moonmoonio@proton.me>
2024-08-17 20:06:11 -0700

    Implement blog post sorting

--- src/lib/components/BlogPostList.svelte
+++ src/lib/components/BlogPostList.svelte
@@ -7,13 +7,24 @@
 	import { hrefConverter } from '$lib/transformers';
 	import { t, locale } from '$lib/translations';
 	import type { BlogPosts } from '$lib/types/BlogPosts';
+	import type { BlogPost } from '$lib/types/BlogPost';
+	import { sortBlogPosts } from '$lib/types/BlogPostMetadata';
 
 	export let blogPosts: BlogPosts;
+	export let numPosts: number | null = null;
+
+	const sortPosts = (posts: [string, BlogPost][]) => {
+		posts = posts.sort((post1, post2) => sortBlogPosts(post1[1].metadata, post2[1].metadata));
+		if (numPosts !== null) {
+			posts = posts.splice(0, numPosts);
+		}
+		return posts;
+	};
 </script>
 
 <div>
 	<Timeline>
-		{#each blogPosts[$locale].entries() as [slug, post]}
+		{#each sortPosts([...blogPosts[$locale].entries()]) as [slug, post]}
 			<div class="m-6 flex w-auto items-center rounded-lg bg-gray-50 py-2 dark:bg-gray-900">
 				<TimelineItem
 					title={post.metadata.title}
--- src/lib/types/BlogPostMetadata.ts
+++ src/lib/types/BlogPostMetadata.ts
@@ -3,3 +3,7 @@ export interface BlogPostMetadata {
 	publishDate: string | Date;
 	summary?: string;
 }
+
+export function sortBlogPosts(post1: BlogPostMetadata, post2: BlogPostMetadata) {
+	return new Date(post2.publishDate).getTime() - new Date(post1.publishDate).getTime();
+}
