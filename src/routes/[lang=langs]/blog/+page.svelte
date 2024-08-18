cc3f6d5a2ac503b79716425ab052865496a7f186
Luna <moonmoonio@proton.me>
2024-08-17 20:33:43 -0700

    Update usage of BlogPostList in blog page

--- src/routes/[lang=langs]/blog/+page.svelte
+++ src/routes/[lang=langs]/blog/+page.svelte
@@ -2,7 +2,7 @@
 	import Heading from 'flowbite-svelte/Heading.svelte';
 	import P from 'flowbite-svelte/P.svelte';
 
-	import { t } from '$lib/translations';
+	import { t, locale } from '$lib/translations';
 	import { posts } from '$lib/blog';
 	import BlogPostList from '$lib/components/BlogPostList.svelte';
 </script>
@@ -10,5 +10,5 @@
 <div class="container mx-auto px-6 py-6 text-xl lg:px-52">
 	<Heading tag="h1">{$t('blogPostList.title')}</Heading>
 	<P class="my-4 text-xl dark:text-gray-400">{$t('blogPostList.description')}</P>
-	<BlogPostList blogPosts={posts} />
+	<BlogPostList blogPosts={[...posts[$locale].values()].map((post) => post.metadata)} />
 </div>
