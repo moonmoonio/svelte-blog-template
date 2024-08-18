76a3730b130c4f9e47dac50c2d4dea73ed304a16
Luna <moonmoonio@proton.me>
2024-08-17 20:32:58 -0700

    Rework BlogPostList to take list of post metadata

--- src/lib/components/BlogPostList.svelte
+++ src/lib/components/BlogPostList.svelte
@@ -6,15 +6,14 @@
 
 	import { hrefConverter } from '$lib/transformers';
 	import { t, locale } from '$lib/translations';
-	import type { BlogPosts } from '$lib/types/BlogPosts';
-	import type { BlogPost } from '$lib/types/BlogPost';
 	import { sortBlogPosts } from '$lib/types/BlogPostMetadata';
+	import type { BlogPostMetadata } from '$lib/types/BlogPostMetadata';
 
-	export let blogPosts: BlogPosts;
+	export let blogPosts: BlogPostMetadata[];
 	export let numPosts: number | null = null;
 
-	const sortPosts = (posts: [string, BlogPost][]) => {
-		posts = posts.sort((post1, post2) => sortBlogPosts(post1[1].metadata, post2[1].metadata));
+	const sortPosts = (posts: BlogPostMetadata[]) => {
+		posts = posts.sort(sortBlogPosts);
 		if (numPosts !== null) {
 			posts = posts.splice(0, numPosts);
 		}
@@ -24,17 +23,17 @@
 
 <div>
 	<Timeline>
-		{#each sortPosts([...blogPosts[$locale].entries()]) as [slug, post]}
+		{#each sortPosts(blogPosts) as metadata}
 			<div class="m-6 flex w-auto items-center rounded-lg bg-gray-50 py-2 dark:bg-gray-900">
 				<TimelineItem
-					title={post.metadata.title}
-					date={new Date(post.metadata.publishDate).toLocaleDateString($locale)}
+					title={metadata.title}
+					date={new Date(metadata.publishDate).toLocaleDateString($locale)}
 				>
-					{#if post.metadata.summary !== undefined}
-						<p class="mb-1 font-normal text-gray-500 dark:text-gray-400">{post.metadata.summary}</p>
+					{#if metadata.summary !== undefined}
+						<p class="mb-1 font-normal text-gray-500 dark:text-gray-400">{metadata.summary}</p>
 					{/if}
 					<div class="pt-2">
-						<Button color="alternative" href={hrefConverter(`/blog/${slug}`, $locale)}
+						<Button color="alternative" href={hrefConverter(`/blog/${metadata.slug}`, $locale)}
 							>{$t('blogPostList.readMore')}<ArrowRightOutline class="ms-2 h-5 w-5" /></Button
 						>
 					</div>
