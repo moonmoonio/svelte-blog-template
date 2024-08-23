87b6d40cdcf76238fbf5f0bc8bd596af70db395a
Luna <moonmoonio@proton.me>
2024-08-22 20:58:38 -0700

    update masonry gallery component

--- src/lib/components/MasonryGallery.svelte
+++ src/lib/components/MasonryGallery.svelte
@@ -1,14 +1,28 @@
 <script lang="ts">
+	import { v4 as uuidv4 } from 'uuid';
 	import BiggerPicture from 'bigger-picture';
 	import type { Image } from '$lib/types/Image';
 	import { onMount } from 'svelte';
 
 	export let images: Image[];
+	const containerId = `${uuidv4()}-images`;
 	let divElement: HTMLDivElement;
 	let bp;
 
-	onMount(() => {
+	onMount(async () => {
 		bp = BiggerPicture({ target: divElement });
+		const { default: Macy } = await import('macy');
+		Macy({
+			container: `#${containerId}`,
+			trueOrder: true,
+			margin: 5,
+			columns: 3,
+			breakAt: {
+				580: {
+					columns: 2
+				}
+			}
+		});
 	});
 
 	function openBiggerPicture(e: Event) {
@@ -21,7 +35,7 @@
 	}
 </script>
 
-<div bind:this={divElement} class="m-4">
+<div bind:this={divElement} class="m-4" id={containerId}>
 	{#each images as image}
 		<a
 			on:click={openBiggerPicture}
