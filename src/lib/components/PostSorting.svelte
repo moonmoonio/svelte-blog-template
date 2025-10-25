c72a8e687588ef2cd750be514ec100d40bb1a08c
Luna <moonmoonio@proton.me>
2025-10-25 14:42:45 -0700

    Add component to sort posts in a list

---
+++ src/lib/components/PostSorting.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import P from 'flowbite-svelte/P.svelte';
+	import Button from 'flowbite-svelte/Button.svelte';
+	import ButtonGroup from 'flowbite-svelte/ButtonGroup.svelte';
+
+	import { t } from '$lib/translations';
+
+	export let newestFirst: boolean;
+</script>
+
+<svelte:head>
+	<title>{$t('blogPostList.title')}</title>
+</svelte:head>
+
+<div>
+	<P>
+		{$t('blogPostList.sortBy')}:
+		<ButtonGroup>
+			<Button
+				color={newestFirst ? 'alternative' : 'green'}
+				on:click={() => (newestFirst = true)}
+				disabled={newestFirst}
+				pill>{$t('blogPostList.newestFirst')}</Button
+			><Button
+				on:click={() => (newestFirst = false)}
+				disabled={!newestFirst}
+				color={!newestFirst ? 'alternative' : 'green'}
+				pill>{$t('blogPostList.oldestFirst')}</Button
+			>
+		</ButtonGroup>
+	</P>
+</div>
+
+<style lang="postcss">
+	div :global(p) {
+		@apply my-4 md:text-xl dark:text-gray-400;
+	}
+</style>
