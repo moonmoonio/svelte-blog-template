71781a99afbad3096fcc3f6eed94c6cddfab9bed
Luna <moonmoonio@proton.me>
2026-07-13 17:42:32 -0600

    Allow dangerous html

--- src/lib/utils/content.ts
+++ src/lib/utils/content.ts
@@ -3,7 +3,7 @@ import { toHtml } from 'hast-util-to-html';
 
 export default function content() {
 	return async function transformer(tree, vFile) {
-		const hast = toHast(tree);
+		const hast = toHast(tree, { allowDangerousHtml: true });
 		const html = toHtml(hast);
 		vFile.data.fm.content = html;
 	};
